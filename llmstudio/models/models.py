6e99ab6e30bebb71c941b58dbea162c0bc10754f
Jose-Bastos <jbastosmiguel@gmail.com>
2023-10-14 16:32:23 +0200

    FEAT: LLMCompare

--- llmstudio/models/models.py
+++ llmstudio/models/models.py
@@ -1,7 +1,14 @@
+import asyncio
+import threading
 from abc import ABC, abstractmethod
+from statistics import mean
+
+import numpy as np
 import requests
 from pydantic import BaseModel
-
+from sentence_transformers import SentenceTransformer, util
+from ..utils.rest_utils import run_apis
+from ..llm_engine.config import LLMEngineConfig, RouteType
 
 class LLMModel(ABC):
     """
@@ -20,10 +27,7 @@ class LLMModel(ABC):
     Methods:
         chat: To be implemented in child classes for providing chatting functionality.
     """
-
-    CHAT_URL = ""
-    TEST_URL = ""
-    EVALUATE_URL = ""
+    PROVIDER = None
 
     @abstractmethod
     def __init__(
@@ -32,7 +36,7 @@ class LLMModel(ABC):
         api_key: str = None,
         api_secret: str = None,
         api_region: str = None,
-        tests: dict = {}
+        llm_engine_config: LLMEngineConfig = LLMEngineConfig()
     ):
         """
         Initialize the LLMModel instance.
@@ -42,13 +46,14 @@ class LLMModel(ABC):
             api_key (str, optional): The API key for authentication. Default is None.
             api_secret (str, optional): The API secret for enhanced security. Default is None.
             api_region (str, optional): The API region for interfacing. Default is None.
-            tests (dict, optional): The batch of tests to be used when evaluating. Default is Empty Dict.
         """
         self.model_name = model_name
         self.api_key = api_key
         self.api_secret = api_secret
         self.api_region = api_region
-        self.tests = tests
+        self.validation_url = f"{str(llm_engine_config.routes_endpoint)}/{RouteType.LLM_VALIDATION.value}/{self.PROVIDER}"
+        self.chat_url = f"{str(llm_engine_config.routes_endpoint)}/{RouteType.LLM_CHAT.value}/{self.PROVIDER}"
+
 
     @staticmethod
     def _raise_api_key_error():
@@ -57,8 +62,9 @@ class LLMModel(ABC):
         )
 
     def _check_api_access(self):
+
         response = requests.post(
-            self.TEST_URL,
+            self.validation_url,
             json={
                 "model_name": self.model_name,
                 "api_key": self.api_key,
@@ -81,9 +87,7 @@ class LLMModel(ABC):
             BaseModel: Validated/adjusted parameters encapsulated in a Pydantic model.
         """
 
-    def chat(
-        self, chat_input: str, parameters: BaseModel = None, is_stream: bool = False
-    ):
+    def chat(self, chat_input: str, parameters: BaseModel = None, is_stream: bool = False):
         """
         Initiate a chat interaction with the language model.
 
@@ -108,9 +112,8 @@ class LLMModel(ABC):
             ValueError: If the API response cannot be parsed or contains error information.
         """
         validated_params = self.validate_parameters(parameters)
-
         response = requests.post(
-            self.CHAT_URL,
+            self.chat_url,
             json={
                 "model_name": self.model_name,
                 "api_key": self.api_key,
@@ -123,34 +126,7 @@ class LLMModel(ABC):
             headers={"Content-Type": "application/json"},
             timeout=10,
         )
-
         return response.json()
-    
-
-    def set_tests(self, tests: dict = {}):
-        self.tests = tests
-
-    def run_tests(self, tests: dict = {}, parameters: dict = {}, is_stream: bool = False):
-
-        if not tests:
-            tests = self.tests
-
-        validated_params = self.validate_parameters(parameters)
-
-        response = requests.post(
-            self.EVALUATE_URL,
-            json={
-                "model_name": self.model_name,
-                "api_key": self.api_key,
-                "api_secret": self.api_secret,
-                "api_region": self.api_region,
-                "tests": tests,
-                "parameters": validated_params,
-                "is_stream": is_stream,
-            },
-            headers={"Content-Type": "application/json"},
-        )
-        return response
 
 
 class LLMClient(ABC):
@@ -173,9 +149,7 @@ class LLMClient(ABC):
 
     MODEL_MAPPING = {}
 
-    def __init__(
-        self, api_key: str = None, api_secret: str = None, api_region: str = None
-    ):
+    def __init__(self, api_key: str = None, api_secret: str = None, api_region: str = None, llm_engine_config: LLMEngineConfig = LLMEngineConfig()):
         """
         Initialize the LLMClient instance.
 
@@ -187,6 +161,8 @@ class LLMClient(ABC):
         self.api_key = api_key
         self.api_secret = api_secret
         self.api_region = api_region
+        self.llm_engine_config = llm_engine_config
+        run_apis(llm_engine_config=self.llm_engine_config)
 
     def get_model(self, model_name: str):
         """
@@ -213,4 +189,125 @@ class LLMClient(ABC):
             api_key=self.api_key,
             api_secret=self.api_secret,
             api_region=self.api_region,
+            llm_engine_config=self.llm_engine_config
         )
+
+
+class LLMCompare(ABC):
+    def __int__(self):
+        pass
+
+    async def _get_response_from_model(self, model: LLMModel, prompt: str, output_dict: dict):
+        """
+        Helper method to get response from a given model and store it in the output dictionary.
+
+        Args:
+            model (LLMModel): The language model to get a response from.
+            prompt (str): The input prompt for the model.
+            output_dict (dict): Dictionary to store the responses.
+
+        Returns:
+            dict: The updated output_dict.
+        """
+        output_dict[model.model_name] = model.chat(prompt)
+        return output_dict
+
+    def _compute_entrywise_average_similarity(self, list1, list2):
+        """
+        Computes cosine average_similarity for each pair of sentences at the same position in two lists.
+
+        Parameters:
+        - list1: List of sentences [s1, s2, ...]
+        - list2: List of sentences [s1, s2, ...]
+
+        Returns:
+        - average_similarity_vector: 1D numpy array with the average_similarity for each respective entry.
+        """
+
+        # initiate an embedding model
+        model = SentenceTransformer("paraphrase-MiniLM-L6-v2")
+
+        # Ensure the two lists are of the same length
+        assert len(list1) == len(list2), "The two lists must be of the same length"
+
+        # Encode the sentences from both lists
+        embeddings1 = model.encode(list1, convert_to_tensor=True)
+        embeddings2 = model.encode(list2, convert_to_tensor=True)
+
+        # Compute average_similarity for respective entries
+        average_similarity_vector = [
+            util.pytorch_cos_sim(emb1, emb2).item() for emb1, emb2 in zip(embeddings1, embeddings2)
+        ]
+
+        return np.array(average_similarity_vector)
+
+    async def _get_llm_performance(self, model, prompt_list, expected_output_list, output_dict):
+
+        latency_list = []
+        cost_list = []
+        out_tokens_list = []
+        chat_output_list = []
+
+        assert len(prompt_list) == len(
+            expected_output_list
+        ), "Prompt List and Expected List are not the same size"
+
+        for prompt in prompt_list:
+            model_response = model.chat(prompt)  # assuming the chat method is asynchronous
+
+            chat_output_list.append(model_response["chatOutput"])
+            latency_list.append(model_response["latency"])
+            cost_list.append(model_response["cost"])
+            out_tokens_list.append(model_response["outputTokens"])
+
+        # now compute some metrics
+        average_latency = mean(latency_list)
+        average_output_token = mean(out_tokens_list)
+        average_cost = mean(cost_list)
+
+        # average_similarity performance
+        average_similarity = mean(
+            self._compute_entrywise_average_similarity(chat_output_list, expected_output_list)
+        )
+
+        statistics = {
+            "average_latency": average_latency,
+            "average_cost": average_cost,
+            "average_output_token": average_output_token,
+            "average_similarity": average_similarity,
+        }
+        output_dict[model.model_name] = statistics
+        return output_dict
+
+    async def single_prompt_compare(self, models: [LLMClient], prompt: str):
+        """
+        Compare multiple language models by obtaining their responses to a given prompt.
+
+        Args:
+            models (list[LLMClient]): List of language models to compare.
+            prompt (str): Input prompt for the models.
+
+        Returns:
+            dict: A dictionary where keys are model names and values are their corresponding responses.
+        """
+
+        output_dict = {}
+
+        tasks = [self._get_response_from_model(model, prompt, output_dict) for model in models]
+
+        await asyncio.gather(*tasks)
+
+        return output_dict
+
+    async def dataset_prompt_compare(self, models, prompt_list, expected_output_list):
+
+        output_dict = {}
+
+        tasks = [
+            self._get_llm_performance(model, prompt_list, expected_output_list, output_dict)
+            for model in models
+        ]
+
+        await asyncio.gather(*tasks)
+
+        return output_dict
