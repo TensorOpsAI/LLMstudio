a4ab11fab58f396ab1383848240d97a27dd401d7
Vasco Reid <vasco.reid@tensorops.ai>
2023-10-16 20:47:14 +0100

    VertexAI Working

--- LLMEngine/providers/vertexai.py
+++ LLMEngine/providers/vertexai.py
@@ -4,11 +4,13 @@ from pydantic import BaseModel, Field, validator
 from typing import Optional
 import tiktoken
 from fastapi.responses import StreamingResponse
-import random, time
+import random
+import time
 from google.oauth2 import service_account
 import vertexai
 from vertexai.language_models import TextGenerationModel, ChatModel, CodeGenerationModel, CodeChatModel
 from LLMEngine.providers.base_provider import BaseProvider
+from LLMEngine.utils import validate_provider_config
 
 # TODO: Change to constants.py
 end_token = "<END_TOKEN>"
@@ -29,18 +31,23 @@ class VertexAIParameters(BaseModel):
     top_p: Optional[float] = Field(1, ge=0, le=1)
     top_k: Optional[float] = Field(40, ge=1, le=40)
 
+
 class VertexAIRequest(BaseModel):
-    api_key: str
+    api_key: dict
     model_name: str
     chat_input: str
     parameters: Optional[VertexAIParameters] = VertexAIParameters()
     is_stream: Optional[bool] = False
 
+
 class VertexAIProvider(BaseProvider):
 
     def __init__(self, config: VertexAIConfig, api_key: dict):
-         super().__init__()
-         self.vertexai_config = validate_provider_config(config, api_key)
+        super().__init__()
+        self.vertexai_config = validate_provider_config(config, api_key)
+    
+
+    # TODO: Request base url and headers based on api_type (not implemented)
 
     async def chat(self, data: VertexAIRequest) -> dict:
         """
@@ -50,8 +57,11 @@ class VertexAIProvider(BaseProvider):
         Returns:
             dict: A dictionary containing the chat input, chat output, tokens data, cost, and other metadata.
         """
-        credentials = service_account.Credentials.from_service_account_info(self.vertexai_config['vertexai_api_key'])
-        vertexai.init(project=data['api_key']["project_id"], credentials=credentials)
+        credentials = service_account.Credentials.from_service_account_info(
+            self.vertexai_config['api_key'])
+        vertexai.init(
+            project=self.vertexai_config['api_key']["project_id"], credentials=credentials)
+        data = VertexAIRequest(**data)
 
         # TODO: Change to constants.py
         model_map = {
@@ -61,8 +71,8 @@ class VertexAIProvider(BaseProvider):
             "codechat-bison": CodeChatModel
         }
         self.validate_model_field(data, model_map.keys())
-        model_class = model_map.get(data['model_name'])
-    
+        model_class = model_map.get(data.model_name)
+
         # TODO: Change to constants.py
         input_arg_name_map = {
             TextGenerationModel: 'prompt',
@@ -74,44 +84,43 @@ class VertexAIProvider(BaseProvider):
         input_arg_name = input_arg_name_map.get(model_class)
 
         kwargs = {
-            "temperature": data['parameters']['temperature'],
-            "max_output_tokens": data['parameters']['max_tokens'],
+            "temperature": data.parameters.temperature,
+            "max_output_tokens": data.parameters.max_tokens,
         }
-        if data['model_name'] not in {"code-bison", "codechat-bison"}:
+        if data.model_name not in {"code-bison", "codechat-bison"}:
             kwargs.update({
-                "top_p": data['parameters']['top_p'],
-                "top_k": data['parameters']['top_k'],
+                "top_p": data.parameters.top_p,
+                "top_k": data.parameters.top_k,
             })
 
         if model_class in {TextGenerationModel, CodeGenerationModel}:
-            model = model_class.from_pretrained(data['model_name'])
+            model = model_class.from_pretrained(data.model_name)
             response = predict(model, input_arg_name,
-                            data['chat_input'], data['is_stream'], **kwargs)
+                               data.chat_input, data.is_stream, **kwargs)
         else:
             model = model_class.from_pretrained(data.model_name)
-            response = chat_predict(model, data['chat_input'],
-                                    data['is_stream'], **kwargs)
+            response = chat_predict(model, input_arg_name,
+                                    data.chat_input, data.is_stream, **kwargs)
 
-        if data['is_stream']:
+        if data.is_stream:
             return StreamingResponse(generate_stream_response(response, data))
 
         data = {
             "id": random.randint(0, 1000),
-            "chatInput": data['chat_input'],
-            "chatOutput": response['text'],
-            "inputTokens": len(data['chat_input']),
-            "outputTokens": len(response['text']),
-            "totalTokens": len(data['chat_input']) + len(response['text']),
-            "cost": get_cost(len(data['chat_input']), len(response['text'])),
+            "chatInput": data.chat_input,
+            "chatOutput": response.text,
+            "inputTokens": len(data.chat_input),
+            "outputTokens": len(response.text),
+            "totalTokens": len(data.chat_input) + len(response.text),
+            "cost": get_cost(len(data.chat_input), len(response.text)),
             "timestamp": time.time(),
-            "modelName": data['model_name'],
-            "parameters": data['parameters'],
+            "modelName": data.model_name,
+            "parameters": data.parameters,
         }
 
         return data
 
 
-    # TODO: Request base url and headers based on api_type (not implemented)
 
 def predict(model, input_str: str, chat_input: str, is_stream: bool, **kwargs):
     """
@@ -147,69 +156,61 @@ def chat_predict(model, input_str: str, chat_input: str, is_stream: bool, **kwar
     chat = model.start_chat()
     if is_stream:
         return chat.send_message_streaming(**args)
-    return model.send_message(**args)
+    return chat.send_message(**args)
+
+
 def get_cost(input_tokens: int, output_tokens: int) -> float:
-        """
-        Calculate the cost of using the OpenAI API based on token usage and model.
-        
-        Args:
-            input_tokens (int): Number of tokens in the input.
-            output_tokens (int): Number of tokens in the output.
-            model_name (str): Identifier of the model used.
-        
-        Returns:
-            float: The calculated cost for the API usage.
-        """
-        return 0.0000005 * (input_tokens + output_tokens)
+    """
+    Calculate the cost of using the OpenAI API based on token usage and model.
+
+    Args:
+        input_tokens (int): Number of tokens in the input.
+        output_tokens (int): Number of tokens in the output.
+        model_name (str): Identifier of the model used.
+
+    Returns:
+        float: The calculated cost for the API usage.
+    """
+    return 0.0000005 * (input_tokens + output_tokens)
+
 
 def get_tokens(chat_input: str, model_name: str) -> int:
-        """
-        Determine the number of tokens in a given input string using the specified model’s tokenizer.
-        
-        Args:
-            chat_input (str): Text to be tokenized.
-            model_name (str): Identifier of the model, determines tokenizer used.
-        
-        Returns:
-            int: Number of tokens in the input string.
-        """
-        tokenizer = tiktoken.encoding_for_model(model_name)
-        return len(tokenizer.encode(chat_input))
+    """
+    Determine the number of tokens in a given input string using the specified model’s tokenizer.
+
+    Args:
+        chat_input (str): Text to be tokenized.
+        model_name (str): Identifier of the model, determines tokenizer used.
+
+    Returns:
+        int: Number of tokens in the input string.
+    """
+    tokenizer = tiktoken.encoding_for_model(model_name)
+    return len(tokenizer.encode(chat_input))
+
 
 def generate_stream_response(response: dict, data: VertexAIProvider):
-        """
-        Generate stream responses, yielding chat output or tokens and cost information at stream end.
-        
-        Args:
-            response (dict): Dictionary containing chunks of responses from the Vertex AI API.
-            data (VertexAIRequest): Object containing necessary parameters for the API call.
-        
-        Yields:
-            str: A chunk of chat output or, at stream end, tokens counts and cost information.
-        """
-        chat_output = ""
+    """
+    Generate stream responses, yielding chat output or tokens and cost information at stream end.
 
-        for chunk in response:
-            print(chunk)
-            print(type(chunk))
-            chat_output += ""
-            yield ""
+    Args:
+        response (dict): Dictionary containing chunks of responses from the Vertex AI API.
+        data (VertexAIRequest): Object containing necessary parameters for the API call.
 
-        input_tokens = len(data.chat_input)
-        output_tokens = len(chat_output)
-        cost = get_cost(input_tokens, output_tokens)
-        yield f"{end_token},{input_tokens},{output_tokens},{cost}"  # json
+    Yields:
+        str: A chunk of chat output or, at stream end, tokens counts and cost information.
+    """
+    chat_output = ""
 
+    for chunk in response:
+        print(chunk)
+        print(type(chunk))
+        chat_output += ""
+        yield ""
 
+    input_tokens = len(data.chat_input)
+    output_tokens = len(chat_output)
+    cost = get_cost(input_tokens, output_tokens)
+    yield f"{end_token},{input_tokens},{output_tokens},{cost}"  # json
 
-# TODO: Send to utils.py
-def validate_provider_config(vertexai_config, api_key):
-    if not (vertexai_config or api_key):
-        raise ValueError(
-            f"Config was not specified neither an api_key was provided."
-        )
-    if vertexai_config is None:
-        vertexai_config = {}
-    vertexai_config.setdefault('vertexai_api_key', api_key)
-    return vertexai_config
 
