d5c8dab7b52da8dce8df9bd2b4706d0e7fd1b189
Vasco Reid <vasco.reid@tensorops.ai>
2023-10-16 20:08:03 +0100

    VertexAI should be working, bug on not getting default AI parameters

--- LLMEngine/providers/vertexai.py
+++ LLMEngine/providers/vertexai.py
@@ -1,6 +1,6 @@
 
 from LLMEngine.config import VertexAIConfig, RouteConfig
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 from typing import Optional
 import tiktoken
 from fastapi.responses import StreamingResponse
@@ -29,62 +29,20 @@ class VertexAIParameters(BaseModel):
     top_p: Optional[float] = Field(1, ge=0, le=1)
     top_k: Optional[float] = Field(40, ge=1, le=40)
 
-class VertexAIProvider(BaseProvider):
+class VertexAIRequest(BaseModel):
     api_key: str
     model_name: str
     chat_input: str
     parameters: Optional[VertexAIParameters] = VertexAIParameters()
     is_stream: Optional[bool] = False
 
-    def __init__(self, config: RouteConfig) -> None:
-        super().__init__(config)
-        if config.model.config is None or not isinstance(config.model.config, VertexAIConfig):
-            # Should be unreachable
-            raise ValueError(
-                "Invalid config type {config.model.config}"
-            )
-        self.vertexai_config: VertexAIConfig = config.model.config
-    
-    # TODO: Request base url and headers based on api_type (not implemented)
+class VertexAIProvider(BaseProvider):
 
-    def predict(model, input_str: str, chat_input: str, is_stream: bool, **kwargs):
-        """
-        Makes a prediction using the specified model.
-        Args:
-            model: The model to use for making predictions.
-            arg_name (str): The name of the argument to pass the chat input as.
-            chat_input (str): The input string for the chat.
-            is_stream (bool): Whether to stream the response.
-            **kwargs: Additional parameters for the prediction function.
-        Returns:
-            The prediction response.
-        """
-        args = {input_str: chat_input, **kwargs}
-        if is_stream:
-            return model.predict_streaming(**args)
-        return model.predict(**args)
+    def __init__(self, config: VertexAIConfig, api_key: dict):
+         super().__init__()
+         self.vertexai_config = validate_provider_config(config, api_key)
 
-
-    def chat_predict(model, input_str: str, chat_input: str, is_stream: bool, **kwargs):
-        """
-        Makes a prediction using the specified chat model.
-        Args:
-            model: The chat model to use for making predictions.
-            arg_name (str): The name of the argument to pass the chat input as.
-            chat_input (str): The input string for the chat.
-            is_stream (bool): Whether to stream the response.
-            **kwargs: Additional parameters for the chat prediction function.
-        Returns:
-            The chat prediction response.
-        """
-        args = {input_str: chat_input, **kwargs}
-        chat = model.start_chat()
-        if is_stream:
-            return chat.send_message_streaming(**args)
-        return model.send_message(**args)
-
-
-    async def chat(self, data) -> dict:
+    async def chat(self, data: VertexAIRequest) -> dict:
         """
         FastAPI endpoint to interact with the VertexAI API for text generation or chat completions.
         Args:
@@ -92,9 +50,8 @@ class VertexAIProvider(BaseProvider):
         Returns:
             dict: A dictionary containing the chat input, chat output, tokens data, cost, and other metadata.
         """
-        credentials = service_account.Credentials.from_service_account_info(
-            self.vertexai_config.vertexai_api_key)
-        vertexai.init(project=data.api_key["project_id"], credentials=credentials)
+        credentials = service_account.Credentials.from_service_account_info(self.vertexai_config['vertexai_api_key'])
+        vertexai.init(project=data['api_key']["project_id"], credentials=credentials)
 
         # TODO: Change to constants.py
         model_map = {
@@ -104,7 +61,7 @@ class VertexAIProvider(BaseProvider):
             "codechat-bison": CodeChatModel
         }
         self.validate_model_field(data, model_map.keys())
-        model_class = model_map.get(data.model_name)
+        model_class = model_map.get(data['model_name'])
     
         # TODO: Change to constants.py
         input_arg_name_map = {
@@ -117,42 +74,80 @@ class VertexAIProvider(BaseProvider):
         input_arg_name = input_arg_name_map.get(model_class)
 
         kwargs = {
-            "temperature": data.parameters.temperature,
-            "max_output_tokens": data.parameters.max_tokens,
+            "temperature": data['parameters']['temperature'],
+            "max_output_tokens": data['parameters']['max_tokens'],
         }
-        if data.model_name not in {"code-bison", "codechat-bison"}:
+        if data['model_name'] not in {"code-bison", "codechat-bison"}:
             kwargs.update({
-                "top_p": data.parameters.top_p,
-                "top_k": data.parameters.top_k,
+                "top_p": data['parameters']['top_p'],
+                "top_k": data['parameters']['top_k'],
             })
 
         if model_class in {TextGenerationModel, CodeGenerationModel}:
-            model = model_class.from_pretrained(data.model_name)
-            response = self.predict(model, input_arg_name,
-                            data.chat_input, data.is_stream, **kwargs)
+            model = model_class.from_pretrained(data['model_name'])
+            response = predict(model, input_arg_name,
+                            data['chat_input'], data['is_stream'], **kwargs)
         else:
             model = model_class.from_pretrained(data.model_name)
-            response = self.chat_predict(model, data.chat_input,
-                                    data.is_stream, **kwargs)
+            response = chat_predict(model, data['chat_input'],
+                                    data['is_stream'], **kwargs)
 
-        if data.is_stream:
+        if data['is_stream']:
             return StreamingResponse(generate_stream_response(response, data))
 
         data = {
             "id": random.randint(0, 1000),
-            "chatInput": data.chat_input,
-            "chatOutput": response.text,
-            "inputTokens": len(data.chat_input),
-            "outputTokens": len(response.text),
-            "totalTokens": len(data.chat_input) + len(response.text),
-            "cost": get_cost(len(data.chat_input), len(response.text)),
+            "chatInput": data['chat_input'],
+            "chatOutput": response['text'],
+            "inputTokens": len(data['chat_input']),
+            "outputTokens": len(response['text']),
+            "totalTokens": len(data['chat_input']) + len(response['text']),
+            "cost": get_cost(len(data['chat_input']), len(response['text'])),
             "timestamp": time.time(),
-            "modelName": data.model_name,
-            "parameters": data.parameters.dict(),
+            "modelName": data['model_name'],
+            "parameters": data['parameters'],
         }
 
         return data
 
+
+    # TODO: Request base url and headers based on api_type (not implemented)
+
+def predict(model, input_str: str, chat_input: str, is_stream: bool, **kwargs):
+    """
+    Makes a prediction using the specified model.
+    Args:
+        model: The model to use for making predictions.
+        arg_name (str): The name of the argument to pass the chat input as.
+        chat_input (str): The input string for the chat.
+        is_stream (bool): Whether to stream the response.
+        **kwargs: Additional parameters for the prediction function.
+    Returns:
+        The prediction response.
+    """
+    args = {input_str: chat_input, **kwargs}
+    if is_stream:
+        return model.predict_streaming(**args)
+    return model.predict(**args)
+
+
+def chat_predict(model, input_str: str, chat_input: str, is_stream: bool, **kwargs):
+    """
+    Makes a prediction using the specified chat model.
+    Args:
+        model: The chat model to use for making predictions.
+        arg_name (str): The name of the argument to pass the chat input as.
+        chat_input (str): The input string for the chat.
+        is_stream (bool): Whether to stream the response.
+        **kwargs: Additional parameters for the chat prediction function.
+    Returns:
+        The chat prediction response.
+    """
+    args = {input_str: chat_input, **kwargs}
+    chat = model.start_chat()
+    if is_stream:
+        return chat.send_message_streaming(**args)
+    return model.send_message(**args)
 def get_cost(input_tokens: int, output_tokens: int) -> float:
         """
         Calculate the cost of using the OpenAI API based on token usage and model.
@@ -206,4 +201,15 @@ def generate_stream_response(response: dict, data: VertexAIProvider):
         yield f"{end_token},{input_tokens},{output_tokens},{cost}"  # json
 
 
+
+# TODO: Send to utils.py
+def validate_provider_config(vertexai_config, api_key):
+    if not (vertexai_config or api_key):
+        raise ValueError(
+            f"Config was not specified neither an api_key was provided."
+        )
+    if vertexai_config is None:
+        vertexai_config = {}
+    vertexai_config.setdefault('vertexai_api_key', api_key)
+    return vertexai_config
 
