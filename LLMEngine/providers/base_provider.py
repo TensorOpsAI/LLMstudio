069694dc8a0f42155c5d3d046b580f33b6403531
Vasco Reid <vasco.reid@tensorops.ai>
2023-10-16 20:47:57 +0100

    Base Provider

--- LLMEngine/providers/base_provider.py
+++ LLMEngine/providers/base_provider.py
@@ -1,32 +1,24 @@
-from typing import Tuple
-from LLMEngine.config import RouteConfig
+from abc import ABC
 
-from pydantic import BaseModel
 
-class BaseProvider(BaseModel):
+class BaseProvider(ABC):
     """
     Base class for LLMStudio LLMEngine providers.
     """
-
-    NAME: str
-    SUPPORTED_ROUTE_TYPES: Tuple[str, ...]
-
-    def __init__(self, config: RouteConfig):
-        self.config = config
+    def __init__(self):
+        super().__init__()
 
     async def chat(self, data) -> dict:
         raise NotImplementedError
 
-    @staticmethod
-    def validate_model_field(data, model_list):
+    def validate_model_field(self, data, model_list):
         from fastapi import HTTPException
-
-        if "model_name" not in data:
+        if not data.model_name:
             raise HTTPException(
                 status_code=422,
                 detail="The parameter 'model_name' is mandatory to be passed in the request body.",
             )
-        if data["model_name"] not in model_list:
+        if data.model_name not in model_list:
             raise HTTPException(
                 status_code=422,
                 detail=f"The model '{data['model_name']}' does not exist.",
