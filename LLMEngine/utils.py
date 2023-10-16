97fe50116529c9311e69b0c4706b3634a23eb8e0
Vasco Reid <vasco.reid@tensorops.ai>
2023-10-16 20:48:12 +0100

    validate_provider_config for providers

--- LLMEngine/utils.py
+++ LLMEngine/utils.py
@@ -17,3 +17,14 @@ def is_valid_endpoint_name(name: str) -> bool:
     """
     pattern = r'[^\w\.-]'
     return re.search(pattern, name) is None
+
+def validate_provider_config(config, api_key):
+    if not (config or api_key):
+        raise ValueError(
+            f"Config was not specified neither an api_key was provided."
+        )
+    if config is None:
+        config = {}
+    if api_key is not None:
+        config.setdefault('api_key', api_key)
+    return config
