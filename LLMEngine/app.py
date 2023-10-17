4b7181b475b5f750bfa64af146bffc108d001312
Vasco Reid <vasco.reid@tensorops.ai>
2023-10-17 10:15:34 +0100

    app - dynamic routing endpoint, based on the type of route to create the endopoint

--- LLMEngine/app.py
+++ LLMEngine/app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException, Request
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, Callable
 
-from LLMEngine.config import LLMEngineConfig, Route
+from LLMEngine.config import LLMEngineConfig, Route, RouteType
 from LLMEngine.constants import LLM_ENGINE_HEALTH_ENDPOINT, LLM_ENGINE_ROUTE_BASE, VERSION
 from LLMEngine.providers import get_provider
 
@@ -16,16 +16,55 @@ class LLMEngineAPI(FastAPI):
         self.dynamic_routes.clear()
         for route in config.routes:
             for provider in route.model_providers:
-                provider_name = provider.provider
-                provider_config = provider.config if provider.config else {}
-                route_and_provider_name = f"{provider_name}-{route.name}"
-                path=f"{LLM_ENGINE_ROUTE_BASE}{route_and_provider_name}"
-                self.add_api_route(
-                    path=path,
-                    endpoint=self._create_dynamic_chat_endpoint(provider_name, provider_config),
-                    methods=["POST"],
+                self._add_dynamic_route(route, provider)
+
+    def _add_dynamic_route(self, route: dict, provider: dict):
+        provider_name = provider.provider
+        provider_config = provider.config or {}
+        route_and_provider_name = f"{provider_name}-{route.name}"
+        path = f"{LLM_ENGINE_ROUTE_BASE}{route_and_provider_name}"
+        
+        self.add_api_route(
+            path=path,
+            endpoint=self._route_type_to_endpoint(
+                provider_name, provider_config, route.route_type
+            ),
+            methods=["POST"],
+        )
+        self.dynamic_routes[route_and_provider_name] = route.to_route(provider_name, path)
+
+    def _route_type_to_endpoint(self, provider_name: str, provider_config: dict, route_type: RouteType) -> Callable:
+        provider_to_factory = {
+            RouteType.LLM_CHAT: "chat",
+            RouteType.LLM_VALIDATION: "test",
+        }
+        
+        factory = provider_to_factory.get(route_type)
+        if factory:
+            return self._create_generic_endpoint(factory,provider_name, provider_config)
+        
+        raise HTTPException(
+            status_code=404,
+            detail=f"Unexpected route type {route_type!r} for provider {provider_name!r}.",
+        )
+
+    def _create_generic_endpoint(self, method_name: str, provider_name: str, provider_config: str) -> Callable:
+        async def _generic_endpoint(request: Request):
+            payload = await request.json()
+            api_key = payload.get('api_key', None)
+            provider_instance = get_provider(provider_name)(provider_config, api_key)
+            method = getattr(provider_instance, method_name, None)
+            
+            if not method:
+                raise HTTPException(
+                    status_code=404,
+                    detail=f"Method {method_name!r} not found for provider {provider_name!r}.",
                 )
-                self.dynamic_routes[route_and_provider_name] = route.to_route(provider_name, path)
+            
+            return await method(payload)
+        
+        return _generic_endpoint
+    
 
     def get_dynamic_route(self, route_name: str) -> Optional[Route]:
         return self.dynamic_routes.get(route_name)
@@ -33,14 +72,37 @@ class LLMEngineAPI(FastAPI):
     def get_dynamic_routes(self):
         return self.dynamic_routes
 
-    def _create_dynamic_chat_endpoint(self, provider_name, provider_config):
-            async def _dynamic_chat_endpoint(request: Request):
-                payload = await request.json()
-                api_key = payload.get('api_key', None)
-                prov = get_provider(provider_name)(provider_config, api_key)
-                return await prov.chat(payload)
-            
-            return _dynamic_chat_endpoint
+def _create_chat_endpoint(provider_name, provider_config):
+        async def _chat_endpoint(request: Request):
+            payload = await request.json()
+            api_key = payload.get('api_key', None)
+            prov = get_provider(provider_name)(provider_config, api_key)
+            return await prov.chat(payload)
+        
+        return _chat_endpoint
+    
+def _create_validation_endpoint(provider_name, provider_config):
+        async def _validation_endpoint(request: Request):
+            payload = await request.json()
+            api_key = payload.get('api_key', None)
+            prov = get_provider(provider_name)(provider_config, api_key)
+            return await prov.test(payload)
+        
+        return _validation_endpoint
+
+def _route_type_to_endpoint(provider_name : str, provider_config : dict, route_type : RouteType):
+    provider_to_factory = {
+        RouteType.LLM_CHAT: _create_chat_endpoint,
+        RouteType.LLM_VALIDATION: _create_validation_endpoint,
+    }
+    if factory := provider_to_factory.get(route_type):
+        return factory(provider_name, provider_config)
+
+    raise HTTPException(
+        status_code=404,
+        detail=f"Unexpected route type {route_type!r} for provider {provider_name!r}.",
+    )
+
 
 def create_app_from_config(config: LLMEngineConfig) -> LLMEngineAPI:
     """
