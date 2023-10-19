1a9c2af31f30f641df20a1df9601fa5f9e3cf6a3
Vasco Reid <vasco.reid@tensorops.ai>
2023-10-19 21:59:24 +0100

    requirments are now correct. NOTE: python version needs to be <=3.11

--- llmstudio/__init__.py
+++ llmstudio/__init__.py
@@ -1,26 +1,24 @@
 name = "version"
-__version__ = "0.1.6"
+__version__ = "0.2.0"
 
 __requirements__ = [
     # core
-    "requests<3",
-    "pydantic==1.10.9",
-    "click",
-    "numpy",
+    "pydantic",
     "requests",
+    "pydantic",
+    "numpy",
     "torch",
     "sentence-transformers",
-    # llm_engine
-    "gunicorn==19.9.0",
-    "openai",
-    "google-auth",
-    "google-cloud-aiplatform",
-    "tiktoken",
     "fastapi",
     "uvicorn",
-    "uuid",
-    "boto3",
-    "PyYaml"
+    "PyYaml",
+    # llm_engine
+    "openai",
+    "tiktoken",
+    "google-auth",
+    "google-cloud-aiplatform",
+    "fastapi",
+    "boto3"
 ]
 
 from .client import LLMStudio
