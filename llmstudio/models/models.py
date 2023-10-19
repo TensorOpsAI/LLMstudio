a7c3043e6a3ab3f0e86a28c8836cbc4f01041295
Vasco Reid <vasco.reid@tensorops.ai>
2023-10-19 21:07:18 +0100

    import fixes

--- llmstudio/models/models.py
+++ llmstudio/models/models.py
@@ -13,7 +13,7 @@ import numpy as np
 import requests
 from pydantic import BaseModel
 from sentence_transformers import SentenceTransformer, util
-from utils.rest_utils import run_apis
+from ..utils.rest_utils import run_apis
 from ..llm_engine.config import LLMEngineConfig, RouteType
 
 class LLMModel(ABC):
