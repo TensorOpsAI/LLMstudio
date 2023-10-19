23dcd1d4fa411b8553054cb6815f03b93d5dc48e
Vasco Reid <vasco.reid@tensorops.ai>
2023-10-19 21:06:39 +0100

    api now runs in the background and doesn't print requests

--- llmstudio/utils/rest_utils.py
+++ llmstudio/utils/rest_utils.py
@@ -17,17 +17,13 @@ def run_llmengine_app(llm_engine_config = LLMEngineConfig()):
     config = _load_route_config(llm_engine_config.config_path)
     llm_engine = create_app_from_config(config)
     print(f"Running LLMEngineAPI on {llm_engine_config.host}:{llm_engine_config.port}")
-    uvicorn.run(llm_engine, host=llm_engine_config.host, port=llm_engine_config.port)
+    uvicorn.run(llm_engine, host=llm_engine_config.host, port=llm_engine_config.port, log_level="critical")
 
 
 def run_apis(llm_engine_config = LLMEngineConfig()):
     threads = []
     if llm_engine_config.localhost and not is_api_running(llm_engine_config.health_endpoint, "LLMEngineAPI"):
         thread = Thread(target = run_llmengine_app, args = (llm_engine_config,))
+        thread.daemon = True
         thread.start()
-        threads.append(thread)
-    
-
-    for thread in threads:
-        thread.join()
 
