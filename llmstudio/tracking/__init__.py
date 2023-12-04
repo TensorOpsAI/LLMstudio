800c5d970eea1a34d820456bf5fd43bf094420c2
D <diogo.goncalves@tensorops.ai>
2023-12-04 22:11:51 +0000

    [chore] precommit hooks

--- llmstudio/tracking/__init__.py
+++ llmstudio/tracking/__init__.py
@@ -34,11 +34,11 @@ def create_tracking_app() -> FastAPI:
     )
 
     app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
+        CORSMiddleware,
+        allow_origins=["http://localhost:3000"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
 
     # Dependency
