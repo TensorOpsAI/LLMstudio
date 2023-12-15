62c8bebafb9038a6431f4831553c923ea869e917
Cláudio Lemos <claudio.lemos@tensorops.ai>
2023-12-15 10:21:53 +0000

    [docs] add pypi readme

--- setup.py
+++ setup.py
@@ -12,6 +12,41 @@ setup(
     },
     author_email="contact@tensorops.ai",
     description="Prompt Perfection at Your Fingertips",
+    long_description="""
+    ## 🚀 Quickstart
+
+    Don't forget to check out [https://docs.llmstudio.ai](docs) page.
+
+    ## Installation
+
+    Install the latest version of **LLMstudio** using `pip`. We suggest that you create and activate a new environment using `conda`
+
+    ```bash
+    pip install llmstudio
+    ```
+
+    Install `bun` if you want to use the UI
+
+    ```bash
+    curl -fsSL https://bun.sh/install | bash
+    ```
+
+    Create a `.env` file at the same path you'll run **LLMstudio**
+
+    ```bash
+    OPENAI_API_KEY="sk-api_key"
+    ANTHROPIC_API_KEY="sk-api_key"
+    ```
+
+    Now you should be able to run **LLMstudio** using the following command.
+
+    ```bash
+    llmstudio server --ui
+    ```
+
+    When the `--ui` flag is set, you'll be able to access the UI at [http://localhost:8000](http://localhost:8000)
+    """,
+    long_description_content_type="text/markdown",
     keywords="ml ai llm llmops openai langchain chatgpt llmstudio tensorops",
     version=SDK_VERSION,
     packages=find_packages(),
