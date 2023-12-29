38f696723c5109569a80633c2a3d17d5ac7dab4e
Cláudio Lemos <claudio.lemos@tensorops.ai>
2023-12-29 18:12:37 +0000

    [feat] new poetry setup

--- llmstudio/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from llmstudio.models.anthropic import AnthropicClient
-from llmstudio.models.bedrock import BedrockClient
-from llmstudio.models.models import LLMClient, LLMCompare, LLMModel
-from llmstudio.models.ollama import OllamaClient
-from llmstudio.models.openai import OpenAIClient
-from llmstudio.models.vertexai import VertexAIClient
