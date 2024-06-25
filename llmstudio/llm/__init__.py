a10a7ddbb8d54c09ff582d643e9698924f440839
diogoazevedo15 <diogoazevedo15@gmail.com>
2024-06-25 15:39:49 +0100

    [refactor] Dealt with lint problems

--- llmstudio/llm/__init__.py
+++ llmstudio/llm/__init__.py
@@ -1,13 +1,14 @@
+import json
+
 import aiohttp
 import requests
 from openai.types.chat import ChatCompletion, ChatCompletionChunk
 
-from llmstudio.cli import start_server
-from llmstudio.config import ENGINE_HOST, ENGINE_PORT
-
 # RESPONSE_PARSER IMPORTS
 from pydantic import BaseModel, ValidationError
-import json
+
+from llmstudio.cli import start_server
+from llmstudio.config import ENGINE_HOST, ENGINE_PORT
 
 
 class LLM:
@@ -107,18 +108,20 @@ class LLM:
                         yield ChatCompletionChunk(**await chunk.decode("utf-8"))
 
     ####################################### RESPONSE PARSER #######################################
-    def parse_response(self,json_str: str, response_model: BaseModel):
+    def parse_response(self, json_str: str, response_model: BaseModel):
         try:
             # Trim the string to start at the first curly bracket and end at the last curly bracket
-            start_index = json_str.find('{')
-            end_index = json_str.rfind('}')
-            trimmed_str = json_str[start_index:end_index+1]
+            start_index = json_str.find("{")
+            end_index = json_str.rfind("}")
+            trimmed_str = json_str[start_index : end_index + 1]
             if start_index == -1 or end_index == -1:
                 raise ValueError("Failed to find JSON in the response.")
             parsed_response = response_model.model_validate(json.loads(trimmed_str))
             return parsed_response
         except json.JSONDecodeError as e:
-            custom_error = Exception(f"JSON string: {trimmed_str}\nFailed to decode JSON string: {str(e)}.")
+            custom_error = Exception(
+                f"JSON string: {trimmed_str}\nFailed to decode JSON string: {str(e)}."
+            )
             return custom_error
         except Exception as e:
             return e
--- llmstudio/parser/__init__.py
+++ llmstudio/parser/__init__.py
@@ -1,20 +1,23 @@
-from pydantic import BaseModel, ValidationError
 import json
 
-class Parser():
-    
-    def parse_response(self,json_str: str, response_model: BaseModel):
+from pydantic import BaseModel, ValidationError
+
+
+class Parser:
+    def parse_response(self, json_str: str, response_model: BaseModel):
         try:
             # Trim the string to start at the first curly bracket and end at the last curly bracket
-            start_index = json_str.find('{')
-            end_index = json_str.rfind('}')
-            trimmed_str = json_str[start_index:end_index+1]
+            start_index = json_str.find("{")
+            end_index = json_str.rfind("}")
+            trimmed_str = json_str[start_index : end_index + 1]
             if start_index == -1 or end_index == -1:
                 raise ValueError("Failed to find JSON in the response.")
             parsed_response = response_model.model_validate(json.loads(trimmed_str))
             return parsed_response
         except json.JSONDecodeError as e:
-            custom_error = Exception(f"Failed to decode JSON string: {str(e)}.\nJSON string: {trimmed_str}\n")
+            custom_error = Exception(
+                f"Failed to decode JSON string: {str(e)}.\nJSON string: {trimmed_str}\n"
+            )
             return custom_error
         except Exception as e:
             return e
