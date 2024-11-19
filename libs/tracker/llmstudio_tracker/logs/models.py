519f42eb09ce9ee369be2cafd4ae11f9034afbca
Clara Gadelho <clara.moreira.2000@gmail.com>
2024-11-19 14:02:19 +0000

    feat: clean db models definition

--- libs/tracker/llmstudio_tracker/logs/models.py
+++ libs/tracker/llmstudio_tracker/logs/models.py
@@ -1,33 +1,38 @@
-from datetime import datetime
-import uuid
-from sqlalchemy import Column, Integer, String, DateTime, JSON, func
+from sqlalchemy import Column, Integer, String, DateTime, JSON
+from datetime import datetime, timezone
 
 from llmstudio_tracker.config import DB_TYPE
-from llmstudio_tracker.db_utils import JSONEncodedDict
 from llmstudio_tracker.database import Base
+from llmstudio_tracker.db_utils import JSONEncodedDict
 
 
 class LogDefault(Base):
     __tablename__ = "logs_default"
 
-    log_id = Column(
-        Integer,
-        primary_key=True,
-        default=lambda: uuid.uuid4().int % 10**12 if DB_TYPE == "bigquery" else None,
-        index=True if DB_TYPE != "bigquery" else False
-    )
-    created_at = Column(
-        DateTime(timezone=True),
-        server_default=func.now() if DB_TYPE != "bigquery" else None,
-        default=datetime.now if DB_TYPE == "bigquery" else None
-    )
-
-    session_id = Column(String)
-    chat_input = Column(String)
-    chat_output = Column(String)
-    context = Column(JSONEncodedDict if DB_TYPE == "bigquery" else JSON)
-    provider = Column(String)
-    model = Column(String)
-    deployment = Column(String)
-    parameters = Column(JSONEncodedDict if DB_TYPE == "bigquery" else JSON)
-    metrics = Column(JSONEncodedDict if DB_TYPE == "bigquery" else JSON)
+    if DB_TYPE == "bigquery":
+        log_id = Column(Integer, primary_key=True, default=lambda: int(
+            datetime.now(timezone.utc).strftime('%Y%m%d%H%M%S%f')))
+        created_at = Column(DateTime(timezone=True),
+                            default=lambda: datetime.now(timezone.utc))
+        session_id = Column(String)
+        chat_input = Column(String)
+        chat_output = Column(String)
+        context = Column(JSONEncodedDict)
+        provider = Column(String)
+        model = Column(String)
+        deployment = Column(String)
+        parameters = Column(JSONEncodedDict)
+        metrics = Column(JSONEncodedDict)
+    else:
+        log_id = Column(Integer, primary_key=True, index=True)
+        created_at = Column(DateTime(timezone=True),
+                            default=lambda: datetime.now(timezone.utc))
+        session_id = Column(String)
+        chat_input = Column(String)
+        chat_output = Column(String)
+        context = Column(JSON)
+        provider = Column(String)
+        model = Column(String)
+        deployment = Column(String)
+        parameters = Column(JSON)
+        metrics = Column(JSON)
--- libs/tracker/llmstudio_tracker/session/models.py
+++ libs/tracker/llmstudio_tracker/session/models.py
@@ -1,6 +1,5 @@
-from datetime import datetime
-import uuid
-from sqlalchemy import JSON, Column, DateTime, Integer, String, func
+from datetime import datetime, timezone
+from sqlalchemy import JSON, Column, DateTime, Integer, String
 
 from llmstudio_tracker.config import DB_TYPE
 from llmstudio_tracker.database import Base
@@ -9,23 +8,19 @@ from llmstudio_tracker.db_utils import JSONEncodedDict
 
 class SessionDefault(Base):
     __tablename__ = "sessions"
-    message_id = Column(
-        Integer,
-        primary_key=True,
-        default=lambda: uuid.uuid4().int % 10**12 if DB_TYPE == "bigquery" else None,
-        index=True if DB_TYPE != "bigquery" else False
-    )
-    session_id = Column(String, index=True if DB_TYPE != "bigquery" else False)
-    chat_history = Column(JSONEncodedDict if DB_TYPE == "bigquery" else JSON)
-    extras = Column(JSONEncodedDict if DB_TYPE == "bigquery" else JSON)
-    updated_at = Column(
-        DateTime(timezone=True),
-        onupdate=datetime.now if DB_TYPE == "bigquery" else func.now(),
-        default=datetime.now if DB_TYPE == "bigquery" else None,
-        server_default=func.now() if DB_TYPE != "bigquery" else None
-    )
-    created_at = Column(
-        DateTime(timezone=True),
-        default=datetime.now if DB_TYPE == "bigquery" else None,
-        server_default=func.now() if DB_TYPE != "bigquery" else None
-    )
+
+    if DB_TYPE == "bigquery":
+        message_id = Column(Integer, primary_key=True, default=lambda: int(datetime.now(timezone.utc).strftime('%Y%m%d%H%M%S%f')))
+        session_id = Column(String)
+        chat_history = Column(JSONEncodedDict)
+        extras = Column(JSONEncodedDict)
+        
+    else:
+        message_id = Column(Integer, primary_key=True, index=True)
+        session_id = Column(String, index=True)
+        chat_history = Column(JSON)
+        extras = Column(JSON)
+
+    updated_at = Column(DateTime(timezone=True), onupdate=lambda: datetime.now(timezone.utc), default=lambda: datetime.now(timezone.utc))
+    created_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
+
