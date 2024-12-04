455d8411cbc956ea9feec09b1d25e26d42e5b40d
Diogo Goncalves <diogoncalves@users.noreply.github.com>
2024-12-04 09:28:56 +0000

    CLI - Proxy & Tracker (#188)
    * Feat/cli for tracker and proxy modules (#184)
    
    * feat: cli for tracker and proxy
    
    * feat: update tracker dockerfile
    
    * style: linting
    
    * chore: bump versions
    
    ---------
    
    Signed-off-by: Clara Moreira Gadelho <56916880+claramoreirag@users.noreply.github.com>
    
    * [fix] bump prerelease version in pyproject.toml
    
    * [fix] bump prerelease version in pyproject.toml
    
    * ci: workflow for pushing alpha versions to dockerhub (#187)
    
    Signed-off-by: Clara Moreira Gadelho <56916880+claramoreirag@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Clara Moreira Gadelho <56916880+claramoreirag@users.noreply.github.com>
    Co-authored-by: Clara Moreira Gadelho <56916880+claramoreirag@users.noreply.github.com>
    Co-authored-by: GitHub Actions <actions@github.com>

---
+++ libs/tracker/llmstudio_tracker/cli.py
@@ -0,0 +1,35 @@
+import os
+import signal
+import threading
+
+import click
+from llmstudio_tracker.server import setup_tracking_server
+
+
+def handle_shutdown(signum, frame):
+    print("Shutting down gracefully...")
+    os._exit(0)
+
+
+@click.group()
+def main():
+    pass
+
+
+@main.command()
+def server():
+    signal.signal(signal.SIGINT, handle_shutdown)
+
+    setup_tracking_server()
+
+    print("Press CTRL+C to stop.")
+
+    stop_event = threading.Event()
+    try:
+        stop_event.wait()
+    except KeyboardInterrupt:
+        print("Shutting down server...")
+
+
+if __name__ == "__main__":
+    main()
