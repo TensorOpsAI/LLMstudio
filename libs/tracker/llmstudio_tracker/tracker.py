80347a1c8ee216aae5fe0ac7613b6037f86f9380
brunoalho <bruno.alho@tensorops.ai>
2026-01-16 16:17:59 +0000

    feat: prompt manager tracker

--- libs/tracker/llmstudio_tracker/tracker.py
+++ libs/tracker/llmstudio_tracker/tracker.py
@@ -2,6 +2,7 @@ import json
 from typing import Optional
 
 import requests
+from llmstudio_tracker.db.schemas.prompt_manager import PromptDefaultBase
 from pydantic import BaseModel
 
 
@@ -74,5 +75,61 @@ class Tracker:
             f"{self.tracking_url}/api/tracking/session/{message_id}",
             headers={"accept": "application/json", "Content-Type": "application/json"},
             timeout=100,
+        )
+        return req
+
+
+class PromptManager:
+    def __init__(self, tracking_config: TrackingConfig):
+        self.tracking_url = tracking_config.url
+        self._session = requests.Session()
+
+    def add_prompt(self, prompt: PromptDefaultBase):
+        req = self._session.post(
+            f"{self.tracking_url}/api/tracking/add/prompt",
+            headers={"accept": "application/json", "Content-Type": "application/json"},
+            data=prompt.model_dump_json(),
+            timeout=100,
+        )
+        return req
+
+    def delete_prompt(self, prompt: PromptDefaultBase):
+        req = self._session.delete(
+            f"{self.tracking_url}/api/tracking/delete/prompt",
+            headers={"accept": "application/json", "Content-Type": "application/json"},
+            data=prompt.model_dump_json(),
+            timeout=100,
+        )
+        return req
+
+    def update_prompt(self, prompt: PromptDefaultBase):
+        req = self._session.patch(
+            f"{self.tracking_url}/api/tracking/update/prompt",
+            headers={"accept": "application/json", "Content-Type": "application/json"},
+            data=prompt.model_dump_json(),
+            timeout=100,
+        )
+        return req
+
+    def get_prompt(
+        self,
+        prompt_id: str = None,
+        name: str = None,
+        model: str = None,
+        provider: str = None,
+    ):
+
+        data = {
+            "prompt_id": prompt_id,
+            "name": name,
+            "model": model,
+            "provider": provider,
+        }
+
+        req = self._session.get(
+            f"{self.tracking_url}/api/tracking/get/prompt",
+            headers={"accept": "application/json", "Content-Type": "application/json"},
+            timeout=100,
+            data=json.dumps(data),
         )
         return req
